2dee30f7b6fedfc176c2e05080e45380b9d9ad91
Ineke Vermeulen <inekevermeulen@gmail.com>
2022-03-25 10:44:58 +0100

    3 rain sensors

--- rain_to_drum/rain_to_drum.ino
+++ rain_to_drum/rain_to_drum.ino
@@ -1,8 +1,11 @@
 #include "volca_beats.h"
 
-#define RAIN_SENS A0
-#define KNOCK A3
-int lastRain = 1024;
+#define RAIN_0 A0
+#define RAIN_1 A1
+#define RAIN_2 A2
+int lastRain0 = 1024;
+int lastRain1 = 1024;
+int lastRain2 = 1024;
 
 void setup() {
   //monitor
@@ -14,10 +17,38 @@ void setup() {
 }
 
 void loop() {
-  int rain = analogRead(RAIN_SENS);
-  int knock = analogRead(KNOCK);
-  Serial.println(knock);
+  int rain_0 = analogRead(RAIN_0);
+  int rain_1 = analogRead(RAIN_1);
+  int rain_2 = analogRead(RAIN_2);
 
+  printValue(0, rain_0, lastRain0);
+  playRain(rain_0, lastRain0);
+  lastRain0 = rain_0;
+
+  printValue(1, rain_1, lastRain1);
+  playRain(rain_1, lastRain1);
+  lastRain1 = rain_1;
+
+  printValue(2, rain_2, lastRain2);
+  playRain(rain_2, lastRain2);
+  lastRain2 = rain_2;
+
+  delay(100);  // delay between reads
+}
+
+void printValue(int pinNr, int rain, int lastRain) {
+  char buffer[40];
+  sprintf(buffer, "%d: %d last value: %d", pinNr, rain, lastRain);
+  Serial.println(buffer);
+}
+
+void sendDrum(int cmd, int pitch, int velocity) {
+  Serial1.write(cmd);
+  Serial1.write(pitch);
+  Serial1.write(velocity);
+}
+
+void playRain(int rain, int lastRain) {
   if ((rain < (lastRain - 100)) || (rain > (lastRain + 100))) {
     if (rain <= 300) {
       sendDrum(NOTE_ON, CL_HAT, 127);
@@ -26,18 +57,5 @@ void loop() {
     } else if (rain > 500 && rain <= 900) {
       sendDrum(NOTE_ON, CRASH, 127);
     }
-    lastRain = rain;
   }
-
-  if(knock > 30) {
-    sendDrum(NOTE_ON, KICK, 127);
-  }
-
-  delay(100);  // delay between reads
-}
-
-void sendDrum(int cmd, int pitch, int velocity) {
-  Serial1.write(cmd);
-  Serial1.write(pitch);
-  Serial1.write(velocity);
 }
